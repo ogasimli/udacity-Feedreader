514270f167a4e21b559b2a378cf9c97d244d03a5
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-04-30 00:30:48 +0400

    test: Add a new test suite named "New Feed Selection"

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -108,9 +108,11 @@ $(function () {
   });
 
   /* TODO: Write a new test suite named "New Feed Selection" */
+  describe('New Feed Selection', function () {
+    /* TODO: Write a test that ensures when a new feed is loaded
+     * by the loadFeed function that the content actually changes.
+     * Remember, loadFeed() is asynchronous.
+     */
 
-  /* TODO: Write a test that ensures when a new feed is loaded
-   * by the loadFeed function that the content actually changes.
-   * Remember, loadFeed() is asynchronous.
-   */
+  });
 }());
