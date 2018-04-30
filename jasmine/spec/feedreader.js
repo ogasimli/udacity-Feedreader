39fcf974310529ff77fe1b2307f3353b1e5065ee
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-04-30 10:27:31 +0400

    refactor: Replace toBeTruthy and toBeFalsy with toBe(true) or toBe(false)

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -40,9 +40,9 @@ $(function () {
     });
 
 
-    /* Test that loops through each feed in the allFeeds object
-     * and ensures it has a name defined and that the name is 
-     * not empty.
+    /* Test that loops through each feed
+     * in the allFeeds object and ensures it has a name defined
+     * and that the name is not empty.
      */
     it('names are defined and not empty', function () {
       allFeeds.forEach(feed => {
@@ -57,31 +57,33 @@ $(function () {
   /* Test suite to check menu */
   describe('The menu', function () {
 
-    /* Test that ensures the menu element is hidden by default.*/
+    /* Test that ensures the menu element is
+     * hidden by default. You'll have to analyze the HTML and
+     * the CSS to determine how we're performing the
+     * hiding/showing of the menu element.
+     */
     it('menu is hidden by default', function () {
       expect($('body').hasClass('menu-hidden')).toBeTruthy();
     });
 
-    /* Test that ensures the menu changes visibility when the menu
-     * icon is clicked. This test should have two expectations: 
-     * does the menu display when clicked and does it hide when
-     * clicked again.
+    /* Test that ensures the menu changes
+     * visibility when the menu icon is clicked. This test
+     * should have two expectations: does the menu display when
+     * clicked and does it hide when clicked again.
      */
     it('menu changes visibility on icon click', function () {
-      // Get HTML body
-      const body = $('body');
       // Get hamburger icon
       const icon = $('.menu-icon-link')
 
       // First click on hamburger icon
       icon.trigger('click')
       // Menu should be visible
-      expect($('body').hasClass('menu-hidden')).toBeFalsy();
+      expect($('body').hasClass('menu-hidden')).toBe(false);
 
       // Second click on hamburger icon
       icon.trigger('click')
       // Menu should be hidden
-      expect($('body').hasClass('menu-hidden')).toBeTruthy();
+      expect($('body').hasClass('menu-hidden')).toBe(true);
     });
   });
 
@@ -92,9 +94,11 @@ $(function () {
       loadFeed(0, done);
     });
 
-    /* Test that ensures when the loadFeed function is called 
-     * and completes its work, there is at least a single .entry
-     * element withfix: in the .feed container.
+    /* Test that ensures when the loadFeed
+     * function is called and completes its work, there is at least
+     * a single .entry element withfix: in the .feed container.
+     * Remember, loadFeed() is asynchronous so this test will require
+     * the use of Jasmine's beforeEach and asynchronous done() function.
      */
     it('at least single entry within container', function () {
       expect($('.feed .entry').length).toBeGreaterThan(0);
@@ -116,8 +120,9 @@ $(function () {
       });
     });
 
-    /* Test that ensures when a new feed is loaded by the loadFeed
-     * function that the content actually changes.
+    /* Test that ensures when a new feed is loaded
+     * by the loadFeed function that the content actually changes.
+     * Remember, loadFeed() is asynchronous.
      */
     it('content changes when new feed loaded', function (done) {
       // Load second feed
