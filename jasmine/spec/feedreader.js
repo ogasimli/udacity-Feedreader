659d45edac4579f1b76d45973e067610ec93975d
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-04-30 10:24:58 +0400

    refactor: Update feedreader.js comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,9 +27,9 @@ $(function () {
     });
 
 
-    /* TODO: Write a test that loops through each feed
-     * in the allFeeds object and ensures it has a URL defined
-     * and that the URL is not empty.
+    /* Test that loops through each feed in the allFeeds object
+     * and ensures it has a URL defined and that the URL is
+     *  not empty.
      */
     it('URLs are defined and not empty', function () {
       allFeeds.forEach(feed => {
@@ -40,9 +40,9 @@ $(function () {
     });
 
 
-    /* TODO: Write a test that loops through each feed
-     * in the allFeeds object and ensures it has a name defined
-     * and that the name is not empty.
+    /* Test that loops through each feed in the allFeeds object
+     * and ensures it has a name defined and that the name is 
+     * not empty.
      */
     it('names are defined and not empty', function () {
       allFeeds.forEach(feed => {
@@ -54,22 +54,18 @@ $(function () {
   });
 
 
-  /* TODO: Write a new test suite named "The menu" */
+  /* Test suite to check menu */
   describe('The menu', function () {
 
-    /* TODO: Write a test that ensures the menu element is
-     * hidden by default. You'll have to analyze the HTML and
-     * the CSS to determine how we're performing the
-     * hiding/showing of the menu element.
-     */
+    /* Test that ensures the menu element is hidden by default.*/
     it('menu is hidden by default', function () {
       expect($('body').hasClass('menu-hidden')).toBeTruthy();
     });
 
-    /* TODO: Write a test that ensures the menu changes
-     * visibility when the menu icon is clicked. This test
-     * should have two expectations: does the menu display when
-     * clicked and does it hide when clicked again.
+    /* Test that ensures the menu changes visibility when the menu
+     * icon is clicked. This test should have two expectations: 
+     * does the menu display when clicked and does it hide when
+     * clicked again.
      */
     it('menu changes visibility on icon click', function () {
       // Get HTML body
@@ -89,25 +85,23 @@ $(function () {
     });
   });
 
-  /* TODO: Write a new test suite named "Initial Entries" */
+  /* Test suite to check initial entries */
   describe('Initial Entries', function () {
 
     beforeEach(function (done) {
       loadFeed(0, done);
     });
 
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element withfix: in the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
+    /* Test that ensures when the loadFeed function is called 
+     * and completes its work, there is at least a single .entry
+     * element withfix: in the .feed container.
      */
     it('at least single entry within container', function () {
       expect($('.entry').length).toBeGreaterThan(0);
     });
   });
 
-  /* TODO: Write a new test suite named "New Feed Selection" */
+  /* Test suite to check loading of new feeds */
   describe('New Feed Selection', function () {
 
     // Variables that will hold old and new feed entries
@@ -122,17 +116,16 @@ $(function () {
       });
     });
 
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
+    /* Test that ensures when a new feed is loaded by the loadFeed
+     * function that the content actually changes.
      */
     it('content changes when new feed loaded', function (done) {
       // Load second feed
       loadFeed(1, function () {
         newFeed = $('.feed').html();
-        // CHeck if oldFeed is defined
+        // Check if oldFeed is defined
         expect(oldFeed).toBeDefined();
-        // CHeck if newFeed is defined
+        // Check if newFeed is defined
         expect(newFeed).toBeDefined();
         // Expect old and new feed not to be equal
         expect(newFeed).not.toEqual(oldFeed);
